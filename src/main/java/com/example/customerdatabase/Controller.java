0ec17b70918caa49e875b63a51ce3155af2e5836
Ghanem <ghanemg@uwindsor.ca>
2021-10-16 03:54:26 -0400

    Test case for customer String

---
+++ src/main/java/com/example/customerdatabase/Controller.java
@@ -0,0 +1,15 @@
+package com.example.customerdatabase;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String CustomerDisplay(){
+        return "Customer go here";
+    }
+
+}
--- src/main/java/com/example/customerdatabase/CustomerDatabaseApplication.java
+++ src/main/java/com/example/customerdatabase/CustomerDatabaseApplication.java
@@ -1,21 +1,32 @@
 package com.example.customerdatabase;
 
+import java.util.*;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class CustomerDatabaseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerDatabaseApplication.class, args);
 	}
-	@GetMapping("/hello")
-	public String helloworld(@RequestParam(value = "name",defaultValue = "world") String name){
-		return String.format("Hello %s!", name);
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+		return args -> {
+			System.out.println("print out beans");
+			String [] bean_names = ctx.getBeanDefinitionNames();
+			Arrays.sort(bean_names);
+			for (String b : bean_names){
+				//System.out.println(b);
+			}
+		};
 	}
 
 }
